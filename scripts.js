110560dc457a6a95709e7cf86e23b47b7d450d2f
Łukasz Jowik <39833065+enigmameister@users.noreply.github.com>
2018-09-04 21:12:47 +0200

    First
    Upload first files of my website

---
+++ scripts.js
@@ -0,0 +1,77 @@
+$("#p-nav").on("click", function() 
+	{
+    $('html,body').animate({
+        scrollTop: $("#portfolio").offset().top},
+        'slow');
+		});
+
+
+$("#c-nav").on("click", function() 
+	{
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
+        'slow');
+		});
+
+
+$("#h-nav").on("click", function() 
+	{
+    $('html,body').animate({
+        scrollTop: $('html').offset().top},
+        'slow');
+		});
+
+
+$("#btn-go").on("click", function() 
+	{
+    $('html,body').animate({
+        scrollTop: $("#portfolio").offset().top},
+        'slow');
+		});
+
+
+$(document).ready(function() {
+    $('#btn-home').click(function(event) {
+        $(".about").animate({'opacity': 0}, 50);
+        event.preventDefault();
+        var url = $(this).attr("index.html .section");
+        $(".about").load("index.html .section", function() {
+            $(this).animate({'opacity': 1}, 1000);
+        });
+    });
+});
+
+
+$(document).ready(function() {
+    $('#btn-about').click(function(event) {
+        $(".section").animate({'opacity': 0}, 50);
+        event.preventDefault();
+        var url = $(this).attr("about.html");
+        $(".section").load("about.html", function() {
+            $(this).animate({'opacity': 1}, 1000);
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#btn-hire").click(function(event){
+        $(".section").animate({'opacity':0}, 50);
+        event.preventDefault();
+        var url = $(this).attr("hire.html");
+        $(".section").load("hire.html", function(){
+            $(this).animate({'opacity':1},1000);
+       });
+    });
+});
+
+$(document).ready(function(){
+                  $('.toggle-menu').click(function(){
+                    $('nav').toggleClass('active')
+                  })
+        })
+
+$(document).ready(function(){
+                  $('nav ul li a').click(function(){
+                    $('nav').removeClass('active')
+                  })
+        })
