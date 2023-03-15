745c5f28f21c072ba6d98cb0bc80698e39b572fa
Łukasz <lukasz.jowik@onet.pl>
2023-03-15 01:34:12 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,28 @@ $(".animation span:nth-child(5)").delay(800).animate({ "left":"0%"}, 700 );
 $(".animation span:nth-child(7)").delay(1600).animate({ "left":"0%"}, 700 );
 $(".animation span:nth-child(8)").delay(1600).animate({ "left":"0%"}, 700 ); 
 
-$("#a-nav").on("click", function()
+$("#a_nav").on("click", function()
 {           
  $('html,body').animate({
      scrollTop: $(".about").offset().top},
      'slow');
 });
 
-$("#p-nav").on("click", function() 
+$("#p_nav").on("click", function() 
 	{
     $('html,body').animate({
         scrollTop: $(".portfolio").offset().top},
         'slow');
 		});
 
-$("#c-nav").on("click", function() 
+$("#c_nav").on("click", function() 
 	{
     $('html,body').animate({
         scrollTop: $(".contact").offset().top},
         'slow');
 		});
 
-$("#h-nav, #btn-c").on("click", function() 
+$("#h_nav, #btn-c").on("click", function() 
 	{
     $('html,body').animate({
         scrollTop: $('html').offset().top},
@@ -115,8 +115,6 @@ $(document).ready(function(){
 })
 
 
-
-
 $(".scroll-up").on("click", function() 
 	{
     $('html,body').animate({
@@ -124,6 +122,7 @@ $(".scroll-up").on("click", function()
         500);
 	});
 
+/* toggle theme/lang */
 $('.theme-picker').hide();
 
 $('.toggle-theme, .theme-closepicker, #theme-red, #theme-lime, #theme-white, #theme-default').on('click', function()
@@ -139,15 +138,163 @@ $('.toggle-lang, .lang-closepicker, #lang-en, #lang-pl, #lang-ua, #lang-de').on(
     $('.toggle-lang, .lang-picker').toggle('1000')
   });
 
-
-  var swapStyleSheet = function (sheet) {
+/* theme change session */
+  var swapStyleSheet = function (sheet) 
+    {
         document.getElementById('theme_css').setAttribute('href', sheet);
         storebackground(sheet);
     }
-    var storebackground = function (swapstylesheet) {
+
+    var storebackground = function (swapstylesheet) 
+    {
         localStorage.setItem("href", swapstylesheet);
     }
-    var loadbackground = function () {
+
+    var loadbackground = function () 
+    {
         document.getElementById('theme_css').setAttribute('href', localStorage.getItem('href'));
     }
     window.onload = loadbackground();
+
+/* lang change 
+https://www.geeksforgeeks.org/how-to-switch-the-language-of-the-page-using-javascript/
+
+*/
+function changeLanguage(lang)
+{
+    location.hash = lang;
+    location.reload();
+}
+    var language = 
+    {
+        en:
+        {
+            hnav: "Home",
+            anav: "About me",
+            pnav: "Portfolio",
+            cnav: "Contact",
+
+            h1: "Welcome",
+            h1about:"About Me",
+            h2portfolio:"Projects",
+            h1contact:"Contant",
+
+            welcome1: "Acuracy."+"Always a clean code",
+            welcome2:"Passion."+"Idea and unique projects",
+            welcome3:"Talk is cheap.Show me the code"
+        },
+
+        pl:
+        {
+            hnav: "Glowna",
+            anav: "O mnie",
+            pnav: "Projekty",
+            cnav: "Kontakt",
+
+            h1about: "O mnie",
+            h2portfolio: "Projekty",
+            h1contact: "Kontakt",
+            h1: "Witaj",
+         
+            welcome1:"Dokładnośc.Zawsze przejrzysty kod",
+            welcome2:"Pasja.Pomysły i unikalne projekty",
+            welcome3:"Rozmowa jest tania.Pokaż mi kod"
+        },
+
+        ua:
+        {
+            hnav: "додому",
+            anav: "про мене",
+            pnav: "портфоліо",
+            cnav: "контакт",
+
+            h1: "Привіт",
+            h1about:"про мене",
+            h1aboutText: 
+            "Привіт, мене звуть Лукаш, мені 29 років."+
+            "У вільний час я люблю програмувати, це моя пристрасть, і я вважаю, що це дуже весело."+
+            "Я не жахливо ставлюся до речей, пов’язаних з комп’ютером, і швидко це розумію."+
+            "Крім роботи у вільний час люблю займатися спортом і слухати «хорошу» музику."+
+            "Насправді нікуди не найму (в ІТ), лише вдосконалюю свої навички та відкриваю нові технології, також люблю програмувати.",
+
+            h2portfolio:"портфолі",
+            h1contact:"онтакт",
+
+            welcome: "Точність.Завжди чистий код"+
+                    "пристрасть.Ідея та унікальні проекти"+
+                    "Розмова дешева.Покажіть мені код"
+        },
+
+        de:
+        {
+            hnav: "Heim",
+            anav: "Über mich",
+            pnav: "Portfolio",
+            cnav: "Kontakt",
+
+            h1: "Hallo",
+            h1about:"Über mich",
+            h1aboutText:
+            "Hallo, mein Name ist Łukasz und ich bin 29 Jahre alt."+
+            "In meiner Freizeit programmiere ich gerne, es ist meine Leidenschaft und ich denke, es macht viel Spaß."+
+            "Ich bin nicht schrecklich in computerbezogenen Dingen und neige dazu, es schnell aufzuheben."+
+            "Neben der Arbeit treibe ich in meiner Freizeit gerne Sport und höre „gute“ Musik."+
+            "Ich stelle eigentlich nirgendwo ein (in der IT), sondern verbessere nur meine Fähigkeiten und entdecke neue Technologien. Programmiere auch gerne.",
+            h2portfolio:"Portfolio",
+            h1contact:"Kontakt",
+
+            welcome: "Genauigkeit.Immer ein sauberer Code"+
+                     "Leidenschaft.Idee und einzigartige Projekte"+
+                      "Reden ist billig.Zeig mir den Code"
+        }
+    };
+
+    if(window.location.hash)
+    {
+        if(window.location.hash=="#pl")
+        {
+        
+            welcomeText.textContent = language.pl.h1;
+
+            h_nav.textContent = language.pl.hnav;
+            a_nav.textContent = language.pl.anav;
+            p_nav.textContent = language.pl.pnav;
+            c_nav.textContent = language.pl.cnav;
+
+            aboutText.textContent = language.pl.h1about;
+            portfolioText.textContent = language.pl.h2portfolio;
+            contactText.textContent = language.pl.h1contact;
+        }
+
+        else if(window.location.hash == "#ua")
+        {
+            welcomeText.textContent = language.ua.h1;
+
+            h_nav.textContent = language.ua.hnav;
+            a_nav.textContent = language.ua.anav;
+            p_nav.textContent = language.ua.pnav;
+            c_nav.textContent = language.ua.cnav;
+
+            aboutText.textContent = language.ua.h1about;
+            aboutText2.textContent = language.ua.h1aboutText;
+            portfolioText.textContent = language.ua.h2portfolio;
+            contactText.textContent = language.ua.h1contact;
+
+        }
+
+        else if(window.location.hash == "#de")
+        {
+            
+            welcomeText.textContent = language.de.h1;
+
+            h_nav.textContent = language.de.hnav;
+            a_nav.textContent = language.de.anav;
+            p_nav.textContent = language.de.pnav;
+            c_nav.textContent = language.de.cnav;
+
+            aboutText.textContent = language.de.h1about;
+            aboutText2.textContent = language.de.h1aboutText;
+            portfolioText.textContent = language.de.h2portfolio;
+            contactText.textContent = language.de.h1contact;
+        }
+    }
